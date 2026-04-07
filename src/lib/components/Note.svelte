f6f74444c3998fe9bbedf1e222105c60c7abe45f
Hat Dealer <jan.richter2007@gmail.com>
2026-04-07 18:29:53 +0200

    fixed deprecated <slot> rendering

--- src/lib/components/Note.svelte
+++ src/lib/components/Note.svelte
@@ -1,6 +1,10 @@
+<script>
+	let { children } = $props();
+</script>
+
 <div class="border-l-4 border-blue-500 bg-gray-900 p-2 rounded-md m-2">
 	<div class="text-lg font-bold text-blue-400 select-none">Note</div>
 	<div class="">
-		<slot></slot>
+		{@render children?.()}
 	</div>
 </div>
--- src/lib/components/Warning.svelte
+++ src/lib/components/Warning.svelte
@@ -1,6 +1,10 @@
+<script>
+	let { children } = $props();
+</script>
+
 <div class="border-l-4 border-yellow-600 bg-amber-950 p-2 rounded-md m-2">
 	<div class="text-lg font-bold text-yellow-600 select-none">Warning!</div>
 	<div class="">
-		<slot></slot>
+		{@render children?.()}
 	</div>
 </div>
